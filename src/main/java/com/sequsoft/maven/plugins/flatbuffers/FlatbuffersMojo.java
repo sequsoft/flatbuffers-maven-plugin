b23c870ab1d71e8bd76c8f477003bf44c8c145f0
Matt Daley <m6daley@gmail.com>
2020-03-30 15:31:59 +0100

    Initial commit, includes getting git repo from github and selecting a specific tag.

---
+++ src/main/java/com/sequsoft/maven/plugins/flatbuffers/FlatbuffersMojo.java
@@ -0,0 +1,112 @@
+package com.sequsoft.maven.plugins.flatbuffers;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Mojo(name = "compile-flatbuffers", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class FlatbuffersMojo extends AbstractMojo {
+
+    private static final String USER_HOME = "user.home";
+    private static final String FB_DIR = ".flatbuffers";
+    private static final String FLATBUFFERS_REPO = "https://github.com/google/flatbuffers.git";
+    private static final String DEFAULT_TAG = "v1.12.0";
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(property = "branch")
+    String tag;
+
+    @Parameter(property = "flatbuffersUrl")
+    String flatbuffersUrl;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        try {
+            String home = getUserHomeDirectory();
+            String fbHome = ensureFlatbuffersDirectory(home);
+            Git repo = ensureFlatbuffersRepository(new File(fbHome));
+            checkoutTag(repo, tag());
+        } catch (FBRuntimeException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    private String getUserHomeDirectory() {
+        String home = System.getProperty(USER_HOME);
+        if (home == null || home.length() == 0) {
+            throw new FBRuntimeException("No user home directory could be found.");
+        }
+        getLog().debug("User home directory = " + home);
+
+        return home;
+    }
+
+    private String ensureFlatbuffersDirectory(String home) {
+        try {
+            Path path = Paths.get(home, FB_DIR);
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+                getLog().info("Created " + FB_DIR + " directory.");
+            } else {
+                getLog().info("Directory " + FB_DIR + " already exists.");
+            }
+
+            return path.toString();
+        } catch (IOException e) {
+            throw new FBRuntimeException(e.getMessage());
+        }
+    }
+
+    private Git ensureFlatbuffersRepository(File dir) {
+        try {
+            getLog().info("Opening flatbuffers git repository in " + dir.toString() + ".");
+            return Git.open(dir);
+        } catch(IOException e) {
+            return closeFlatbuffersRepository(dir);
+        }
+    }
+
+    private String flatbuffersUrl() {
+        return flatbuffersUrl == null ? FLATBUFFERS_REPO : flatbuffersUrl;
+    }
+
+    private String tag() {
+        return tag == null ? DEFAULT_TAG : tag;
+    }
+
+    private Git closeFlatbuffersRepository(File dir) {
+        try {
+            String url = flatbuffersUrl();
+            getLog().info("Cloning flatbuffers repository from " + url + ".");
+            return Git.cloneRepository()
+                    .setURI(flatbuffersUrl())
+                    .setDirectory(dir)
+                    .call();
+        } catch (GitAPIException e) {
+            throw new FBRuntimeException("Could not clone repository " + FLATBUFFERS_REPO + ".", e);
+        }
+    }
+
+    private void checkoutTag(Git repo, String tag) {
+        try {
+            repo.checkout().setName(tag).call();
+        } catch (GitAPIException e) {
+            throw new FBRuntimeException("Could not checkout tag " + tag + ".", e);
+        }
+    }
+}
---
+++ src/test/java/com/sequsoft/maven/plugins/flatbuffers/PluginTest.java
@@ -0,0 +1,11 @@
+package com.sequsoft.maven.plugins.flatbuffers;
+
+import org.junit.Test;
+
+public class PluginTest {
+
+    @Test
+    public void first_test() throws Exception {
+
+    }
+}
